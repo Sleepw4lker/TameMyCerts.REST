ff2f7b3d15fcc5350b5e4287be5ec397217b780b
Uwe Gradenegger <info@gradenegger.eu>
2026-09-03 09:22:07 +0200

    Update auxiliary files

--- src/TameMyCerts.REST/AutoVersionIncrement.cs
+++ src/TameMyCerts.REST/AutoVersionIncrement.cs
@@ -10,5 +10,5 @@ using System.Reflection;
 //      Build Number
 //      Revision
 
-[assembly: AssemblyVersion("1.1.351.835")]
-[assembly: AssemblyFileVersion("1.1.351.835")]
+[assembly: AssemblyVersion("1.2.1341.391")]
+[assembly: AssemblyFileVersion("1.2.1341.391")]
